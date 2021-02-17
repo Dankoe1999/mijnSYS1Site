2e337609e4c2448a279eec993622aa4f70b2277f
Dankoe1999 <duncanspiegelaar@gmail.com>
2021-02-17 02:16:42 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hallo</title>
+  </head>
+  <body>
+
+    <?php echo "<p>Hallo</p>"; ?>
+
+  </body>
+</html>
